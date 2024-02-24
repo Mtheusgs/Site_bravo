befd97b70d74b73a083ce2fa13cd1da916fa1ac3
Matheus Garcia Sampaio <148104090+Mtheusgs@users.noreply.github.com>
2024-02-24 01:52:03 -0300

    Add files via upload

---
+++ Bravo/main.js
@@ -0,0 +1,80 @@
+  var textos = [
+    "Fornecer a melhor experiência de usuário a seus clientes por meio de hardware, software e serviços inovadores.",
+    "Acreditamos que estamos na face da terra para fazer ótimos produtos e isso não vai mudar. Estamos aqui para fazer os melhores produtos do mundo e deixar o mundo melhor do que o encontramos.",
+    "Ouvir e relaciona de maneira amigável com o cliente pra tornar tudo amigável, auxiliando no aprendizado.E sempre prezando por respeito e trasparência."
+  ];
+
+  var titulos = ["Missão", "Visão", "Valores"];
+
+  var indiceAtual = 0;
+
+  function mudarTextoETitulo() {
+    
+    document.getElementById("textoMudavel").style.opacity = 0;
+    document.getElementById("mudarTitulo").style.opacity = 0;
+
+    setTimeout(function() {
+      
+      document.getElementById("textoMudavel").textContent = textos[indiceAtual];
+      document.getElementById("mudarTitulo").textContent = titulos[indiceAtual];
+
+      
+
+      
+      indiceAtual = (indiceAtual + 1) % textos.length;
+
+      
+      document.getElementById("textoMudavel").style.opacity = 1;
+      document.getElementById("mudarTitulo").style.opacity = 1;
+    }, 500); // 500ms = tempo da transição
+  }  
+mudarTextoETitulo();
+setInterval(mudarTextoETitulo, 3000);  
+
+let count = 1;
+document.getElementById("slide1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 2000) 
+
+function nextImage(){
+  count++;
+  if(count>4){
+    count=1
+  } 
+
+  document.getElementById("slide"+count).checked = true;
+}
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 4,
+  spaceBetween : 25,
+  loop: true, 
+  centerSlide: 'true',
+  fade:'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, 
+    DynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+
+  breakpoint:{
+    0:{
+      slidesPerView: 1,
+
+    },
+    520:{
+      slidesPerView: 2,
+    }, 
+    920:{
+      slidesPerView: 3,
+    },
+  },
+});
+
